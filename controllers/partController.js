de6060af8f1252a39b7ae7aa40c667ed64b52161
Calvin Buckle <ccccbuckle@gmail.com>
2024-09-10 11:26:28 +0200

    create part post

--- controllers/partController.js
+++ controllers/partController.js
@@ -1,5 +1,5 @@
 const Part = require('../models/part');
-
+const { body, validationResult } = require('express-validator');
 const asyncHandler = require('express-async-handler');
 
 
@@ -25,12 +25,53 @@ exports.part_detail = asyncHandler( async(req,res,next) => {
 });
 
 exports.part_create_get = asyncHandler( async(req,res,next) => {
-    res.send("Not Implemented: part create get")
+    res.render("part_form", {title: "Add a new part"})
 });
 
-exports.part_create_post = asyncHandler(async(req,res,next) => {
-    res.send("Not implemented: part create post")
-});
+exports.part_create_post = [
+    body('partNumber', 'part name must contain atleast 3 characters')
+    .trim()
+    .isLength({min : 3})
+    .escape(),
+
+    body('oem', 'OEM must contain at least 3 characters')
+    .trim()
+    .isLength({min:3})
+    .escape(),
+
+
+    body('quantity', 'Quantity must be a number greater than 0')
+    .isInt({gt:0}),
+
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+        
+        const part = new Part({ partNumber: req.body.partNumber, oem: req.body.oem, quantity: req.body.quantity });
+    
+        if (!errors.isEmpty()) {
+            res.render("part_form", {
+                title: "Create Part",
+                partNumber: partNumber,
+                oem: oem,
+                quantity: quantity,
+                errors: errors.array(),
+            });
+            return;
+        } else {
+            const partExists = await Part.findOne({ partNumber: req.body.partNumber })
+                .collation({ locale: "en", strength: 2 })
+                .exec();
+    
+            if (partExists) {
+                res.redirect(partExists.url);
+            } else {
+                await part.save();
+                res.redirect(`/catalog/parts/${part._id}`);
+            }
+        }
+    })
+];
+
 
 exports.part_delete_get = asyncHandler(async(req,res,next) => {
     res.send("Not implemented: part delete get")
